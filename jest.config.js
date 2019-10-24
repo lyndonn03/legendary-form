d63ec7b4ba4f7d76576bea0bafdecd1bb0b3eea2
Lyndonn Pamintuan <lpamintuan@medgrocer.com>
2019-10-24 11:43:25 +0000

    [CHORE]: add coveralls

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: "ts-jest",
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "/__tests__/.*\\.test.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["<rootDir>/setupTests.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/*.{ts,tsx}",
+    "!app/react/__tests__/api/api-test-helpers.ts"
+  ]
+};
--- test/index.test.js
+++
@@ -1,5 +0,0 @@
-describe("Init Commit Test", () => {
-  it("Should pass because this is an init commit", () => {
-    expect("working").toBeTruthy();
-  });
-});
